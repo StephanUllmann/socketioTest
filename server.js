c1d38842bca4227c8deda150cfa5c331b318275b
Stephan Ullmann <117584384+StephanUllmann@users.noreply.github.com>
2023-06-08 21:42:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://main--luxury-mochi-9f35c6.netlify.app/",
+      "https://luxury-mochi-9f35c6.netlify.app/",
     ],
   },
 });
